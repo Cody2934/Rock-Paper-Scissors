3aab9d36e04f9b326fd9a7362e50e66385db7771
Cody Brown <cody6lefever@gmail.com>
2020-01-16 12:00:39 -0800

    added more pics and cleaned up typos

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 //need imports
-import { getRandomThrow, getThrowFromNumber, checkResult } from './throwfunction.js';
+import { getRandomThrow, checkResult } from './throwfunction.js';
 
 const button = document.getElementById('submit-button');
 const resultDisplay = document.getElementById('result-display');
-const resultTally = document.getElementById('result-tally');
 const drawsDisplay = document.getElementById('draws');
 const winsDisplay = document.getElementById('wins');
 const lossesDisplay = document.getElementById('losses');
+const haha = document.getElementById('haha');
+const booya = document.getElementById('booya');
+const again = document.getElementById('again');
+
 
 let draws = 0;
 let wins = 0;
@@ -17,19 +20,33 @@ button.addEventListener('click', () => {
     const userInput = document.querySelector('input:checked');
     const userChoice = userInput.value;
     const computerChoice = getRandomThrow();
-    const resultTally = checkResult(userChoice, computerChoice);
+    const result = checkResult(userChoice, computerChoice);
     
-    if (resultTally === 'win') {
+    if (computer === 'rock') return booya.classList.remove('hidden');
+
+
+    if (result === 'win') {
         wins ++;
         winsDisplay.textContent = wins;
-        resultDisplay.textContent = 'You win!';
-    } else if (resultTally === 'lose') {
+        resultDisplay.textContent = 'Good Throw! You win!';
+        booya.classList.remove('hidden');
+        haha.classList.add('hidden');
+        again.classList.add('hidden');
+    } else if (result === 'lose') {
         losses++;
         lossesDisplay.textContent = losses;
-        resultDisplay.textContent = 'You lose!';
+        resultDisplay.textContent = 'You lose! Try Again!';
+        haha.classList.remove('hidden');
+        booya.classList.add('hidden');
+        again.classList.add('hidden');
     } else {
         draws++;
         drawsDisplay.textContent = draws;
-        resultDisplay.textContent = 'Draw!';
+        resultDisplay.textContent = 'You Both Threw the Same! Draw!';
+        again.classList.remove('hidden');
+        booya.classList.add('hidden');
+        haha.classList.add('hidden');
     }
 });
+
+
